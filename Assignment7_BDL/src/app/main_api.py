21166b0832ec59c8a38665427926bd067a6c2689
Srivas S <me20b175@smail.iitm.ac.in>
2024-05-17 22:33:59 +0530

    FInal commit

--- Assignment7_BDL/src/app/main_api.py
+++ Assignment7_BDL/src/app/main_api.py
@@ -25,9 +25,6 @@ model_path = sys.argv[1] if len(sys.argv) > 1 else None
 app = FastAPI()
 model=None
 Instrumentator().instrument(app).expose(app)
-'''def load_model_keras(path: str) -> Sequential:
-    model = load_model(path) #Load the model from keras
-    return model'''
 def predict_digit(data_point:list)->str:
     data_point = np.array(data_point, dtype=np.float32) / 255.0 #Data normalisation
     data_point = data_point.reshape(1, -1) #reshaping to None,784
@@ -53,33 +50,35 @@ async def predict_digit_api(request: Request,upload_file: UploadFile = File(...)
     #if not model:
         #return {"error": "Model is not loaded."}
     contents = await upload_file.read()
-    client_ip = request.client.host
+    client_ip = request.client.host #gets the client IP
     # Open the image using PIL
-    API_USAGE_COUNTER.labels(client_ip=client_ip).inc()
+    API_USAGE_COUNTER.labels(client_ip=client_ip).inc() #Count the API calls
     image = Image.open(io.BytesIO(contents)).convert('L') #Converts image to black and white
     # Resize the image to 28x28
     image=format_image(image)
     # Convert the image to a numpy array
     image_array = np.array(image)
-    cpu_percent=psutil.cpu_percent(interval=1)
+    input_len=len(image_array)
+    cpu_percent=psutil.cpu_percent(interval=1) #Get the CPU usage percentage
     CPU_UTIL_TIME.labels(client_ip=client_ip).set(cpu_percent) #Change name to percentage
-    memory_info = psutil.virtual_memory()
-    MEMORY_UTILIZATION_GAUGE.labels(client_ip=client_ip).set(memory_info.percent)
-    net_io = psutil.net_io_counters()
-    NETWORK_IO_BYTES_GAUGE.labels(client_ip=client_ip).set(net_io.bytes_sent + net_io.bytes_recv)
-    NETWORK_IO_BYTES_RATE_GAUGE.labels(client_ip=client_ip).set((net_io.bytes_sent + net_io.bytes_recv) / (time.time() - start_time))
+    memory_info = psutil.virtual_memory() #Get the memory information
+    MEMORY_UTILIZATION_GAUGE.labels(client_ip=client_ip).set(memory_info.percent) #Set the memory utilization
+    net_io = psutil.net_io_counters()   #Get the network I/O
+    NETWORK_IO_BYTES_GAUGE.labels(client_ip=client_ip).set(net_io.bytes_sent + net_io.bytes_recv) #Adds the sent and received network bytes
+    NETWORK_IO_BYTES_RATE_GAUGE.labels(client_ip=client_ip).set((net_io.bytes_sent + net_io.bytes_recv) / (time.time() - start_time)) #Calculates the rate
     # Flatten the image array to a 1D array
     data_point = image_array.flatten().tolist()
     prediction = predict_digit(data_point)
-    processing_time = calculate_processing_time(start_time, len(data_point))
-    PROCESSING_TIME_GAUGE.labels(client_ip=client_ip).set(processing_time)
+    processing_time = calculate_processing_time(start_time, len(data_point)) #Get the processing time
+    PROCESSING_TIME_GAUGE.labels(client_ip=client_ip).set(processing_time) #Store it for prometheus
     # Calculate API runtime
     api_runtime = time.time() - start_time
-    API_RUNTIME_GAUGE.labels(client_ip=client_ip).set(api_runtime)
-    
+    API_RUNTIME_GAUGE.labels(client_ip=client_ip).set(api_runtime) #Get the run time
+    api_tltime=api_runtime/input_len
     # Calculate API T/L time (assuming T/L time is the total time for processing this request)
-    API_TL_TIME_GAUGE.labels(client_ip=client_ip).set(api_runtime)
+    API_TL_TIME_GAUGE.labels(client_ip=client_ip).set(api_tltime)
     return {"predicted_digit": prediction}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000) #This sets up host in 127.0.0.1:8000
